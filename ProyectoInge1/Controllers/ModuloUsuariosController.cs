3515f5b76e0b1ba94ca080eb6b6b23baabecc447
JoseDanielSC <josedaniel2214@gmail.com>
2016-10-02 19:28:23 -0600

    Módulo de Usuarios 2

---
+++ ProyectoInge1/Controllers/ModuloUsuariosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoInge1.Models;
+
+namespace ProyectoInge1.Controllers
+{
+    public class ModuloUsuariosController : Controller
+    {
+        Entities baseDatos = new Entities();
+
+        public ActionResult Index()
+        {
+            ModeloIntermedio modelo = new ModeloIntermedio();
+            modelo.listaPersonas = baseDatos.Usuario.ToList();
+            return View(modelo);
+        }
+
+    }
+}
