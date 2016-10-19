611ef45132dc918c4140640e2b961db7e77f51cd
Andreína Alvarado <andreinaraccoon@hotmail.com>
2016-10-19 10:37:51 -0600

    Scripts SQL

--- Creación de Tablas Sprint 2.sql
+++ Creación de Tablas Sprint 2.sql
@@ -2,9 +2,9 @@ USE BD_IngeGrupo1;
 
 CREATE TABLE Proyecto (
 	Id				VARCHAR(20),
-    Nombre   		VARCHAR(50),
+    Nombre   		VARCHAR(50) 		NOT NULL,
     Descripcion		VARCHAR(100),
-	Estado			VARCHAR(20),
+	Estado			VARCHAR(20)			NOT NULL,
     FechaInicio		DATE,
     FechaFinal		DATE,
 	Duracion		VARCHAR(10),
@@ -15,7 +15,7 @@ CREATE TABLE Usuarios_asociados_proyecto(
 	CedulaUsuario	VARCHAR(16),
 	IdUsuario		NVARCHAR(128),
 	IdProyecto		VARCHAR(20),
-	RolProyecto		VARCHAR(15),
+	RolProyecto		VARCHAR(15)			NOT NULL,
 	CONSTRAINT	PK_Usuario_asociados_proyecto PRIMARY KEY (CedulaUsuario, IdUsuario, IdProyecto), 
 	CONSTRAINT	FK_Usuario FOREIGN KEY (CedulaUsuario, IdUsuario) REFERENCES Usuario(Cedula, Id),
 	CONSTRAINT	FK_Proyecto FOREIGN KEY (IdProyecto) REFERENCES Proyecto(Id)
@@ -23,7 +23,7 @@ CREATE TABLE Usuarios_asociados_proyecto(
 
 CREATE TABLE Requerimiento(
 	Id				VARCHAR(20),
-	Nombre			VARCHAR(40),
+	Nombre			VARCHAR(40)			NOT NULL,
 	Prioridad		VARCHAR(2),
 	Esfuerzo		VARCHAR(2),
 	Estado			VARCHAR(20),
@@ -40,9 +40,9 @@ CREATE TABLE Requerimiento(
 
 CREATE TABLE Cambio(
 	Fecha			DATE,
-	Descripcion		VARCHAR(200),
-	Justificacion	VARCHAR(200),
-	IdRequerimiento	VARCHAR(20),
+	Descripcion		VARCHAR(200)		NOT NULL,
+	Justificacion	VARCHAR(200)		NOT NULL,
+	IdRequerimiento	VARCHAR(20)			NOT NULL,
 	CONSTRAINT	PK_Cambio PRIMARY KEY (Fecha, IdRequerimiento),
 	CONSTRAINT	FK_Cambio FOREIGN KEY (IdRequerimiento) REFERENCES Requerimiento(Id) 
 );
@@ -58,8 +58,8 @@ CREATE TABLE Usuarios_asociados_cambios(
 
 CREATE TABLE CriterioAceptacion(
 	Escenario		VARCHAR(30),
-	Descripcion		VARCHAR(200),
-	IdRequerimiento VARCHAR(20)
+	Descripcion		VARCHAR(200)		NOT NULL,
+	IdRequerimiento VARCHAR(20),
 	CONSTRAINT	PK_CriterioAceptacion PRIMARY KEY (Escenario, IdRequerimiento),
 	CONSTRAINT	FK_Requerimiento FOREIGN KEY (IdRequerimiento) REFERENCES Requerimiento(Id)
 );
