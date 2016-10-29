7ba2b20f260094efafdf29827a15e85fb4dbba09
Isabel Chaves <ichaves94@gmail.com>
2016-10-29 14:02:10 -0600

    Controladora del Modulo de Requerimientos

--- ProyectoInge1/Controllers/ModuloRequerimientosController.cs
+++ ProyectoInge1/Controllers/ModuloRequerimientosController.cs
@@ -19,10 +19,9 @@ namespace ProyectoInge1.Controllers
         Entities baseDatos = new Entities();
         ApplicationDbContext context = new ApplicationDbContext();
 
-        public ActionResult Index(string sortOrder, string tipo, string currentFilter, string searchString, int? page, string param)
+        public ActionResult Index(string sortOrder, string tipo, string currentFilter, string searchString, int? page, string Proyecto)
         {
-            ViewBag.pro = new SelectList(baseDatos.Proyecto, "Id", "Nombre");
-
+            ViewBag.pro = new SelectList(baseDatos.Proyecto, "Id", "Nombre", Proyecto);
             ViewBag.CurrentSort = sortOrder;
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
@@ -39,104 +38,103 @@ namespace ProyectoInge1.Controllers
             ViewBag.CurrentFilter = searchString;
 
             var requerimiento = from s in baseDatos.Requerimiento
-                           select s;
+                                select s;
 
-            if (!String.IsNullOrEmpty(param))
+            requerimiento = requerimiento.Where(s => s.IdProyecto.Equals(Proyecto));
+
+            if (!String.IsNullOrEmpty(searchString))
             {
-                requerimiento = requerimiento.Where(s => s.IdProyecto.Contains(param));
+                requerimiento = requerimiento.Where(s => s.Id.Contains(searchString) || s.Nombre.Contains(searchString)
+                                             || s.Prioridad.Contains(searchString) || s.Esfuerzo.Contains(searchString)
+                                             || s.Estado.Contains(searchString) || s.Sprint.Contains(searchString)
+                                             || s.Modulo.Contains(searchString));
+            }
 
-                if (!String.IsNullOrEmpty(searchString))
-                {
-                    requerimiento = requerimiento.Where(s => s.Id.Contains(searchString) || s.Nombre.Contains(searchString)
-                                           || s.Prioridad.Contains(searchString) || s.Esfuerzo.Contains(searchString)
-                                           || s.Estado.Contains(searchString) || s.Sprint.Contains(searchString)
-                                           || s.Modulo.Contains(searchString));
-                }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    switch (tipo)
+                    {
+                        case "Id":
+                            requerimiento = requerimiento.OrderByDescending(s => s.Id);
+                            break;
+                        case "Nombre":
+                            requerimiento = requerimiento.OrderByDescending(s => s.Nombre);
+                            break;
+                        case "Prioridad":
+                            requerimiento = requerimiento.OrderByDescending(s => s.Prioridad);
+                            break;
+                        case "Esfuerzo":
+                            requerimiento = requerimiento.OrderByDescending(s => s.Esfuerzo);
+                            break;
+                        case "Sprint":
+                            requerimiento = requerimiento.OrderByDescending(s => s.Sprint);
+                            break;
+                        case "Modulo":
+                            requerimiento = requerimiento.OrderByDescending(s => s.Modulo);
+                            break;
+                        default:
+                            requerimiento = requerimiento.OrderByDescending(s => s.Id);
+                            break;
+                    }
+                    break;
+                case "Date":
+                    switch (tipo)
+                    {
+                        case "FechaInicio":
+                            requerimiento = requerimiento.OrderBy(s => s.FechaInicio);
+                            break;
+                        case "FechaFinal":
+                            requerimiento = requerimiento.OrderBy(s => s.FechaFinal);
+                            break;
+                        default:
+                            requerimiento = requerimiento.OrderBy(s => s.FechaInicio);
+                            break;
+                    }
+                    break;
+                case "date_desc":
+                    switch (tipo)
+                    {
+                        case "FechaInicio":
+                            requerimiento = requerimiento.OrderByDescending(s => s.FechaInicio);
+                            break;
+                        case "FechaFinal":
+                            requerimiento = requerimiento.OrderByDescending(s => s.FechaFinal);
+                            break;
+                        default:
+                            requerimiento = requerimiento.OrderByDescending(s => s.FechaInicio);
+                            break;
+                    }
+                    break;
+                default:
+                    switch (tipo)
+                    {
+                        case "Id":
+                            requerimiento = requerimiento.OrderBy(s => s.Id);
+                            break;
+                        case "Nombre":
+                            requerimiento = requerimiento.OrderBy(s => s.Nombre);
+                            break;
+                        case "Prioridad":
+                            requerimiento = requerimiento.OrderBy(s => s.Prioridad);
+                            break;
+                        case "Esfuerzo":
+                            requerimiento = requerimiento.OrderBy(s => s.Esfuerzo);
+                            break;
+                        case "Sprint":
+                            requerimiento = requerimiento.OrderBy(s => s.Sprint);
+                            break;
+                        case "Modulo":
+                            requerimiento = requerimiento.OrderBy(s => s.Modulo);
+                            break;
+                        default:
+                            requerimiento = requerimiento.OrderBy(s => s.Id);
+                            break;
+                    }
+                    break;
+
             }
-                switch (sortOrder)
-                {
-                    case "name_desc":
-                        switch (tipo)
-                        {
-                            case "Id":
-                                requerimiento = requerimiento.OrderByDescending(s => s.Id);
-                                break;
-                            case "Nombre":
-                                requerimiento = requerimiento.OrderByDescending(s => s.Nombre);
-                                break;
-                            case "Prioridad":
-                                requerimiento = requerimiento.OrderByDescending(s => s.Prioridad);
-                                break;
-                            case "Esfuerzo":
-                                requerimiento = requerimiento.OrderByDescending(s => s.Esfuerzo);
-                                break;
-                            case "Sprint":
-                                requerimiento = requerimiento.OrderByDescending(s => s.Sprint);
-                                break;
-                            case "Modulo":
-                                requerimiento = requerimiento.OrderByDescending(s => s.Modulo);
-                                break;
-                            default:
-                                requerimiento = requerimiento.OrderByDescending(s => s.Id);
-                                break;
-                        }
-                        break;
-                    case "Date":
-                        switch (tipo)
-                        {
-                            case "FechaInicio":
-                                requerimiento = requerimiento.OrderBy(s => s.FechaInicio);
-                                break;
-                            case "FechaFinal":
-                                requerimiento = requerimiento.OrderBy(s => s.FechaFinal);
-                                break;
-                            default:
-                                requerimiento = requerimiento.OrderBy(s => s.FechaInicio);
-                                break;
-                        }
-                        break;
-                    case "date_desc":
-                        switch (tipo)
-                        {
-                            case "FechaInicio":
-                                requerimiento = requerimiento.OrderByDescending(s => s.FechaInicio);
-                                break;
-                            case "FechaFinal":
-                                requerimiento = requerimiento.OrderByDescending(s => s.FechaFinal);
-                                break;
-                            default:
-                                requerimiento = requerimiento.OrderByDescending(s => s.FechaInicio);
-                                break;
-                        }
-                        break;
-                    default:
-                        switch (tipo)
-                        {
-                            case "Id":
-                                requerimiento = requerimiento.OrderBy(s => s.Id);
-                                break;
-                            case "Nombre":
-                                requerimiento = requerimiento.OrderBy(s => s.Nombre);
-                                break;
-                            case "Prioridad":
-                                requerimiento = requerimiento.OrderBy(s => s.Prioridad);
-                                break;
-                            case "Esfuerzo":
-                                requerimiento = requerimiento.OrderBy(s => s.Esfuerzo);
-                                break;
-                            case "Sprint":
-                                requerimiento = requerimiento.OrderBy(s => s.Sprint);
-                                break;
-                            case "Modulo":
-                                requerimiento = requerimiento.OrderBy(s => s.Modulo);
-                                break;
-                            default:
-                                requerimiento = requerimiento.OrderBy(s => s.Id);
-                                break;
-                        }
-                        break;
-                
-            }
+
             int pageSize = 7;
             int pageNumber = (page ?? 1);
 
@@ -225,7 +223,7 @@ namespace ProyectoInge1.Controllers
                 modelo.accion = Accion;
                 return View(modelo);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.Write(e);
                 return View("Index");
