bad4d3c132ebec57eff6ca65424e4152a775ad6a
Andreína Alvarado <andreinaraccoon@hotmail.com>
2016-11-15 22:54:41 -0600

    Cambios en el módulo de solicitud de cambios

--- ProyectoInge1/Controllers/ModuloCambiosController.cs
+++ ProyectoInge1/Controllers/ModuloCambiosController.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using ProyectoInge1.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace ProyectoInge1.Controllers
 {
-    public class ModuloCambiosController : Controller{}
+    public class ModuloCambiosController : Controller{
+
+        Entities baseDatos = new Entities();
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public ActionResult Edit(ModeloProyecto modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                baseDatos.Entry(modelo.modeloCambio).State = EntityState.Modified;
+                baseDatos.SaveChanges();
+            }
+            else
+            {
+
+            }
+        }
+
+    }
 }
