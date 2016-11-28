c2618a3f362cc950762f9304934ae91440fbdf82
JoseDanielSC <josedaniel2214@gmail.com>
2016-11-28 17:13:09 +0100

    IndexHistorialCambios

--- ProyectoInge1/Controllers/ModuloRequerimientosController.cs
+++ ProyectoInge1/Controllers/ModuloRequerimientosController.cs
@@ -417,19 +417,176 @@ namespace ProyectoInge1.Controllers
         -------------------------------------------------------------------------------------------*/
 
         /* Método que carga el modelo para la vista Index de Historial de Cambios
-         * Requiere: Tipo de ordenamiento, tipo de filtrado, patron de búsqueda, número de página y id del proyecto
-         * Modifica: el modelo
-         * Retorna: el modelo de páginación cargado
-         */
-        public ActionResult IndexHistorialCambios(string sortOrder, string tipo, string currentFilter, string searchString, int? page)
+ * Requiere: Tipo de ordenamiento, tipo de filtrado, patron de búsqueda, número de página y id del proyecto
+ * Modifica: el modelo
+ * Retorna: el modelo de páginación cargado
+ */
+
+        public ActionResult IndexHistorialCambios(string sortOrder, string tipo, string currentFilter, string searchString, int? page, string Requerimiento, string Proyecto)
         {
-            var historialCambios = from s in baseDatos.Cambio
-                                   select s;
+
+            // Id del usuario en sessión 
+            String id = System.Web.HttpContext.Current.User.Identity.GetUserId();
+            // Rol del usuario en sessión
+            String rol = context.Users.Find(id).Roles.First().RoleId;
+
+            // Se filtran todos los proyectos
+            var proyecto = from Proyecto p in baseDatos.Proyecto
+                           select p;
+
+            // Si no es administrador se filtran solo los proyectos a los cuales pertecene
+            if (!(rol == "01Admin"))
+            {
+                proyecto = from Proyecto p in baseDatos.Proyecto
+                           join Usuarios_asociados_proyecto USP in baseDatos.Usuarios_asociados_proyecto on
+                            p.Id equals USP.IdProyecto
+                           where USP.IdUsuario == id
+                           select p;
+
+            }
+
+            // Se filtran los cambios asociados a un proyectos
+            var cambios = from Cambio c in baseDatos.Cambio
+                          where c.IdRequerimiento == Requerimiento && c.IdProyecto == Proyecto && c.VersionCambio != null
+                          select c;
+
+            // Modificaciones a los atributos de los cambios
+            foreach (var i in cambios)
+            {
+                if (i.VersionCambio == null)
+                    i.VersionCambio = -1;
+
+                var nombre = from Usuario u in baseDatos.Usuario
+                             join Cambio c in baseDatos.Cambio on
+                             u.Id equals c.SolicitanteCambio
+                             where u.Id == i.SolicitanteCambio
+                             select u;
+
+                foreach (var j in nombre)
+                {
+                    i.SolicitanteCambio = j.NombreCompleto;
+                }
+            }
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+
+            ViewBag.CurrentFilter = searchString;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                cambios = cambios.Where(s => s.IdSolicitante.Contains(searchString) || s.Nombre.Contains(searchString)
+                                              || s.Prioridad.Contains(searchString) || s.Esfuerzo.Contains(searchString)
+                                              || s.Estado.Contains(searchString) || s.Sprint.Contains(searchString)
+                                              || s.Modulo.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    switch (tipo)
+                    {
+                        case "Id":
+                            cambios = cambios.OrderByDescending(s => s.IdSolicitud);
+                            break;
+                        case "Nombre":
+                            cambios = cambios.OrderByDescending(s => s.Nombre);
+                            break;
+                        case "Prioridad":
+                            cambios = cambios.OrderByDescending(s => s.Prioridad);
+                            break;
+                        case "Esfuerzo":
+                            cambios = cambios.OrderByDescending(s => s.Esfuerzo);
+                            break;
+                        case "Sprint":
+                            cambios = cambios.OrderByDescending(s => s.Sprint);
+                            break;
+                        case "Modulo":
+                            cambios = cambios.OrderByDescending(s => s.Modulo);
+                            break;
+                        case "Version":
+                            cambios = cambios.OrderByDescending(s => s.Version);
+                            break;
+                        default:
+                            cambios = cambios.OrderByDescending(s => s.IdSolicitud);
+                            break;
+                    }
+                    break;
+                case "Date":
+                    switch (tipo)
+                    {
+                        case "FechaInicio":
+                            cambios = cambios.OrderBy(s => s.FechaInicio);
+                            break;
+                        case "FechaFinal":
+                            cambios = cambios.OrderBy(s => s.FechaFinal);
+                            break;
+                        default:
+                            cambios = cambios.OrderBy(s => s.FechaInicio);
+                            break;
+                    }
+                    break;
+                case "date_desc":
+                    switch (tipo)
+                    {
+                        case "FechaInicio":
+                            cambios = cambios.OrderByDescending(s => s.FechaInicio);
+                            break;
+                        case "FechaFinal":
+                            cambios = cambios.OrderByDescending(s => s.FechaFinal);
+                            break;
+                        default:
+                            cambios = cambios.OrderByDescending(s => s.FechaInicio);
+                            break;
+                    }
+                    break;
+                default:
+                    switch (tipo)
+                    {
+                        case "IdSolicitud":
+                            cambios = cambios.OrderBy(s => s.IdSolicitud);
+                            break;
+                        case "Nombre":
+                            cambios = cambios.OrderBy(s => s.Nombre);
+                            break;
+                        case "Prioridad":
+                            cambios = cambios.OrderBy(s => s.Prioridad);
+                            break;
+                        case "Esfuerzo":
+                            cambios = cambios.OrderBy(s => s.Esfuerzo);
+                            break;
+                        case "Sprint":
+                            cambios = cambios.OrderBy(s => s.Sprint);
+                            break;
+                        case "Modulo":
+                            cambios = cambios.OrderBy(s => s.Modulo);
+                            break;
+                        case "Version":
+                            cambios = cambios.OrderBy(s => s.Version);
+                            break;
+                        default:
+                            cambios = cambios.OrderBy(s => s.IdSolicitud);
+                            break;
+                    }
+                    break;
+
+            }
 
             int pageSize = 7;
             int pageNumber = (page ?? 1);
 
-            return View(historialCambios.ToPagedList(pageNumber, pageSize));
+            return View(cambios.ToPagedList(pageNumber, pageSize));
         }
 
         /* Método que carga el modelo para la vista Index de Solicitudes de Cambios
